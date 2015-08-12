0e8007db8eeac6657008703fdeda1e1a64d940f2
dmytroivo <dmytro.ivo@gmail.com>
2015-08-12 10:48:22 +0300

    Многопоточное суммирование

--- src/pack/ArrayThread.java
+++ src/pack/ArrayThread.java
@@ -3,21 +3,21 @@ package pack;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ArrayThread implements Runnable {
-	
-	private static AtomicInteger qSum=new AtomicInteger(0);
-	
-	public static int getQSum(){
+
+	private static AtomicInteger qSum = new AtomicInteger(0);
+
+	public static int getQSum() {
 		return qSum.get();
 	}
-	
+
 	private int[] arr;
 	private int index;
 	private int qThread;
 
 	ArrayThread(int[] arr, int index, int qThread) {
 		this.arr = arr;
-		this.index=index;
-		this.qThread=qThread;
+		this.index = index;
+		this.qThread = qThread;
 	}
 
 	public void run() {
@@ -26,11 +26,11 @@ public class ArrayThread implements Runnable {
 
 	private int subsum(int index) {
 		int sum = 0;
-		int maxIndex = (index + 2*arr.length/qThread) > arr.length ? arr.length
-				: index + arr.length/qThread;
+		int maxIndex = (index + 2 * arr.length / qThread) > arr.length ? arr.length
+				: index + arr.length / qThread;
 		for (int i = index; i < maxIndex; i++) {
 			sum += arr[i];
 		}
-		return sum;	
+		return sum;
 	}
 }
--- src/pack/Main.java
+++ src/pack/Main.java
@@ -1,45 +1,44 @@
 package pack;
 
-
 import java.util.Random;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-    int[] arr=new int[70000000];
-    int sum=0;
-    int qThread=3;
-    
-    Random r=new Random();
-    for(int i=0;i<arr.length;i++){
-    arr[i]=r.nextInt(50);	
-    }
-    
-    Thread[] athr=new Thread[qThread];
-   
-    long time1=System.currentTimeMillis();
-    for (int i=0, j=0; i<qThread;i++,j+=arr.length/qThread){
-    athr[i] = new Thread(new ArrayThread(arr, j, qThread));
-    athr[i].start();
-    }
-    
-    try {
-    	for(int i=0; i<qThread;i++){
-		athr[i].join();
-    	}
+		int[] arr = new int[70000000];
+		int sum = 0;
+		int qThread = 3;
+
+		Random r = new Random();
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = r.nextInt(50);
+		}
+
+		Thread[] athr = new Thread[qThread];
+
+		long time1 = System.currentTimeMillis();
+		for (int i = 0, j = 0; i < qThread; i++, j += arr.length / qThread) {
+			athr[i] = new Thread(new ArrayThread(arr, j, qThread));
+			athr[i].start();
+		}
+
+		try {
+			for (int i = 0; i < qThread; i++) {
+				athr[i].join();
+			}
 		} catch (InterruptedException e) {
-		System.out.println(e);
-	}
-    long time2=System.currentTimeMillis();
-	System.out.printf("%d поток: сумма= %,d / %tL ms\n",qThread,ArrayThread.getQSum(),time2-time1);
-    
-	
-	time1=System.currentTimeMillis();	
-	for(int x:arr){
-		sum+=x;
-	}
-	time2=System.currentTimeMillis();
-	System.out.printf("1 поток: сумма= %,d / %tL ms\n",sum,time2-time1);
+			System.out.println(e);
+		}
+		long time2 = System.currentTimeMillis();
+		System.out.printf("%d поток: сумма= %,d / %tL ms\n", qThread,
+				ArrayThread.getQSum(), time2 - time1);
+
+		time1 = System.currentTimeMillis();
+		for (int x : arr) {
+			sum += x;
+		}
+		time2 = System.currentTimeMillis();
+		System.out.printf("1 поток: сумма= %,d / %tL ms\n", sum, time2 - time1);
 	}
 
 }
